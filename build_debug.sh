3e5f42a7e16d3a8f978840f61284a0139422df95
elmattic <elmattic@users.noreply.github.com>
2020-09-07 23:34:22 +0200

    Add updated scripts

--- build_debug.sh
+++ build_debug.sh
@@ -1,1 +1,1 @@
-cargo --verbose rustc --lib -- -C target-feature=+avx2,+bmi2
+cargo --verbose rustc --lib -- -C target-feature=+avx2,+bmi2,+lzcnt
--- build_release.sh
+++ build_release.sh
@@ -1,1 +1,1 @@
-cargo --verbose rustc --lib --release -- -C target-feature=+avx2,+bmi2 -C llvm-args=-align-all-nofallthru-blocks=5
+cargo --verbose rustc --lib --release -- -C target-feature=+avx2,+bmi2,+lzcnt -C llvm-args=-align-all-nofallthru-blocks=5
